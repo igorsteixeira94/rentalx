7a15866f5742c04cb16ea6a741f9c5cbdc6e8217
Igor Rodrigues <igorsteixeira94@gmail.com>
2021-04-27 23:58:30 -0300

    feat: Model and Repository Categories

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,35 @@
+import Category from '../model/Category';
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoryRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    return this.categories.find((category) => category.name === name);
+  }
+}
+
+export { CategoryRepository };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
+import { categoriesRouter } from './routes/categories.route';
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Word' });
-});
+app.use('/categories', categoriesRouter);
 
 app.listen(3333, () => {
   console.log('Server is Running 🚀️🚀️');
